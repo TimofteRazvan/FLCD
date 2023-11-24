8ce23173ff6d90398bb6369dd0075b9fd80fb000
StIoan <64016407+StIoan@users.noreply.github.com>
2023-11-24 12:35:33 +0200

    Add files via upload

---
+++ lab2/Lab3/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Scanner scanner = new Scanner();
+        // scanner.readFile("Lab3/src/Lab1-1err.txt");
+        // scanner.display("Lab3/src/PIF.out", "Lab3/src/ST.out");
+        FiniteAutomata fa = new FiniteAutomata();
+        fa.start();
+        // fa.read();
+        // fa.display();
+
+        // String userInput = "011";
+
+        // if (fa.isValidSequence(userInput)) {
+        //     System.out.println("The sequence " + userInput + " is valid.");
+        // } else {
+        //     System.out.println("The sequence " + userInput + " is not valid.");
+        // }
+    }
+}
